0585b811d856ef009e68ed83cf1b10514e1b8b5f
dave <zevakato@gmail.com>
2023-04-01 18:53:33 +0200

    update 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,193 +34,7 @@ const tokenId = 0;
 
 const Home: NextPage = () => {
   const address = useAddress();
-  const [quantity, setQuantity] = useState(1);
-  const { contract: editionDrop } = useContract(myEditionDropContractAddress);
-  const { data: contractMetadata } = useContractMetadata(editionDrop);
 
-  const claimConditions = useClaimConditions(editionDrop);
-  const activeClaimCondition = useActiveClaimConditionForWallet(
-    editionDrop,
-    address,
-    tokenId
-  );
-  const claimerProofs = useClaimerProofs(editionDrop, address || "", tokenId);
-  const claimIneligibilityReasons = useClaimIneligibilityReasons(
-    editionDrop,
-    {
-      quantity,
-      walletAddress: address || "",
-    },
-    tokenId
-  );
-
-  const claimedSupply = useTotalCirculatingSupply(editionDrop, tokenId);
-
-  const totalAvailableSupply = useMemo(() => {
-    try {
-      return BigNumber.from(activeClaimCondition.data?.availableSupply || 0);
-    } catch {
-      return BigNumber.from(1_000_000);
-    }
-  }, [activeClaimCondition.data?.availableSupply]);
-
-  const numberClaimed = useMemo(() => {
-    return BigNumber.from(claimedSupply.data || 0).toString();
-  }, [claimedSupply]);
-
-  const numberTotal = useMemo(() => {
-    const n = totalAvailableSupply.add(BigNumber.from(claimedSupply.data || 0));
-    if (n.gte(1_000_000)) {
-      return "";
-    }
-    return n.toString();
-  }, [totalAvailableSupply, claimedSupply]);
-
-  const priceToMint = useMemo(() => {
-    const bnPrice = BigNumber.from(
-      activeClaimCondition.data?.currencyMetadata.value || 0
-    );
-    return `${utils.formatUnits(
-      bnPrice.mul(quantity).toString(),
-      activeClaimCondition.data?.currencyMetadata.decimals || 18
-    )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
-  }, [
-    activeClaimCondition.data?.currencyMetadata.decimals,
-    activeClaimCondition.data?.currencyMetadata.symbol,
-    activeClaimCondition.data?.currencyMetadata.value,
-    quantity,
-  ]);
-
-  const maxClaimable = useMemo(() => {
-    let bnMaxClaimable;
-    try {
-      bnMaxClaimable = BigNumber.from(
-        activeClaimCondition.data?.maxClaimableSupply || 0
-      );
-    } catch (e) {
-      bnMaxClaimable = BigNumber.from(1_000_000);
-    }
-
-    let perTransactionClaimable;
-    try {
-      perTransactionClaimable = BigNumber.from(
-        activeClaimCondition.data?.maxClaimablePerWallet || 0
-      );
-    } catch (e) {
-      perTransactionClaimable = BigNumber.from(1_000_000);
-    }
-
-    if (perTransactionClaimable.lte(bnMaxClaimable)) {
-      bnMaxClaimable = perTransactionClaimable;
-    }
-
-    const snapshotClaimable = claimerProofs.data?.maxClaimable;
-
-    if (snapshotClaimable) {
-      if (snapshotClaimable === "0") {
-        // allowed unlimited for the snapshot
-        bnMaxClaimable = BigNumber.from(1_000_000);
-      } else {
-        try {
-          bnMaxClaimable = BigNumber.from(snapshotClaimable);
-        } catch (e) {
-          // fall back to default case
-        }
-      }
-    }
-
-    let max;
-    if (totalAvailableSupply.lt(bnMaxClaimable)) {
-      max = totalAvailableSupply;
-    } else {
-      max = bnMaxClaimable;
-    }
-
-    if (max.gte(1_000_000)) {
-      return 1_000_000;
-    }
-    return max.toNumber();
-  }, [
-    claimerProofs.data?.maxClaimable,
-    totalAvailableSupply,
-    activeClaimCondition.data?.maxClaimableSupply,
-    activeClaimCondition.data?.maxClaimablePerWallet,
-  ]);
-
-  const isSoldOut = useMemo(() => {
-    try {
-      return (
-        (activeClaimCondition.isSuccess &&
-          BigNumber.from(activeClaimCondition.data?.availableSupply || 0).lte(
-            0
-          )) ||
-        numberClaimed === numberTotal
-      );
-    } catch (e) {
-      return false;
-    }
-  }, [
-    activeClaimCondition.data?.availableSupply,
-    activeClaimCondition.isSuccess,
-    numberClaimed,
-    numberTotal,
-  ]);
-
-  const canClaim = useMemo(() => {
-    return (
-      activeClaimCondition.isSuccess &&
-      claimIneligibilityReasons.isSuccess &&
-      claimIneligibilityReasons.data?.length === 0
-    );
-  }, [activeClaimCondition.isSuccess, claimIneligibilityReasons.isSuccess, claimIneligibilityReasons.data?.length]);
-
-  const isLoading = useMemo(() => {
-    return (
-      activeClaimCondition.isLoading || claimedSupply.isLoading || !editionDrop
-    );
-  }, [activeClaimCondition.isLoading, editionDrop, claimedSupply.isLoading]);
-
-  const buttonLoading = useMemo(
-    () => isLoading || claimIneligibilityReasons.isLoading,
-    [claimIneligibilityReasons.isLoading, isLoading]
-  );
-  const buttonText = useMemo(() => {
-    if (isSoldOut) {
-      return "Sold Out";
-    }
-
-    if (canClaim) {
-      const pricePerToken = BigNumber.from(
-        activeClaimCondition.data?.currencyMetadata.value || 0
-      );
-      if (pricePerToken.eq(0)) {
-        return "Mint (1$ + gas)";
-      }
-      return `Mint (${priceToMint})`;
-    }
-    if (claimIneligibilityReasons.data?.length) {
-      return parseIneligibility(claimIneligibilityReasons.data, quantity);
-    }
-    if (buttonLoading) {
-      return "Checking eligibility...";
-    }
-
-    return "Claiming not available";
-  }, [
-    isSoldOut,
-    canClaim,
-    claimIneligibilityReasons.data,
-    buttonLoading,
-    activeClaimCondition.data?.currencyMetadata.value,
-    priceToMint,
-    quantity,
-  ]);
-
-
-
-  // function handleButtonClick() {
-  //   window.location.href = '/home';
-  // }
   function handleButtonClick2() {
     window.location.href = '/courses';
   }
@@ -245,6 +59,15 @@ const Home: NextPage = () => {
     window.location.href = '/home';
   }
 
+  function redirectToCoursesPage(imageNumber: number) {
+    if (imageNumber === 1) {
+      window.location.href = '/courses';
+    } else if (imageNumber === 2) {
+      window.location.href = '/page2';
+    } else if (imageNumber === 3) {
+      window.location.href = '/page3';
+    }
+  }
   
 
   return (
@@ -305,7 +128,7 @@ const Home: NextPage = () => {
                     </li>
                     <li>
                       <ConnectWallet
-                        accentColor="#a9a9a9"
+                        accentColor="#252525"
                         colorMode="dark"
                         btnTitle="Connect Wallet"
                       />
@@ -314,7 +137,7 @@ const Home: NextPage = () => {
                 )}
                 <div className="hidden md:block">
                   <ConnectWallet
-                    accentColor="black"
+                    accentColor="#252525"
                     colorMode="light"
                     btnTitle="Connect Wallet"
                   />
@@ -323,13 +146,166 @@ const Home: NextPage = () => {
               
           </div>
       </nav>
-
-      <div className='w-9/12 m-auto'>
+      <div className='w-[80%] h-[60em] m-auto items-center flex'>
+        <div className='w-[35em] h-60 items-center ml-20 '>
+            <p className='font-mono text-xl'>First Web3 online courses platform</p><br></br>
+            <br></br>
+            <p className='font-mono text-[2.5em]'>Bringing Web3 To Learning</p><br></br>
+            <br></br>
+            <p className='font-mono text-xl'>Skills for your present (and your future). Get started with us.</p>
+        </div>
+      </div>
+      <div className='w-full'>
+        <div className='w-[65%] m-auto items-center flex '>
+          <p className='font-mono text-[1.6em]'>Trending Courses:</p><br></br>
+        </div>
+      </div>
+      <div className='w-[65%] h-[30em] m-auto items-center flex bg-[#252525] border-2 border-[grey] hover:bg-[#171717] cursor-pointer' onClick={() => redirectToCoursesPage(1)}>
+        <img
+          key={'https://external-preview.redd.it/1GIbkRiU0HE6PVtBxBNa7mD2EpA_7rA9ZjDAQDpPsfU.jpg?width=640&crop=smart&auto=webp&s=a49ffb84576197ab0692feaf58e84e389d140f76'}
+          src={'https://external-preview.redd.it/1GIbkRiU0HE6PVtBxBNa7mD2EpA_7rA9ZjDAQDpPsfU.jpg?width=640&crop=smart&auto=webp&s=a49ffb84576197ab0692feaf58e84e389d140f76'}
+          alt="placeholder"
+          className='h-[20em] rounded-xl ml-20'
+        />   
+        <div>
+          <p className='leading-10 w-full text-3xl font-mono ml-20'>
+            React Native Complete Guide 2023: Zero to Mastery
+          </p>
+          <p className=' w-full text-base font-mono ml-20 leading-6 mt-6'>
+          Everything about React Native - build cross-platform enterprise apps, incl. Hooks,<br /> Redux, Firebase, Rest API, Publishing <br />
+          Tools Tools Tools Tools Tools Tools Tools Tools <br />
+          </p>
+          <p className='leading-10 w-full text-xs font-mono ml-20'>
+          By Dave De Cooper
+          </p>
+          <p className='leading-10 w-full text-3xl font-mono ml-20 mt-32'>
+          $16.99
+          </p>
+        </div>
+      </div>
+      <div className='w-6/12 m-auto'>
         <div className="grid grid-cols-4 gap-4">
-          <div className='bg-black h-[20em] w-56 rounded-3xl'>01</div>
-          <div className='bg-white h-[20em] w-56 rounded-3xl'>02</div>
-          <div className='bg-black h-[20em] w-56 rounded-3xl'>03</div>
-          <div className='bg-white h-[20em] w-56 rounded-3xl'>04</div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer' onClick={() => redirectToCoursesPage(1)}>
+            <img
+              key={'https://external-preview.redd.it/1GIbkRiU0HE6PVtBxBNa7mD2EpA_7rA9ZjDAQDpPsfU.jpg?width=640&crop=smart&auto=webp&s=a49ffb84576197ab0692feaf58e84e389d140f76'}
+              src={'https://external-preview.redd.it/1GIbkRiU0HE6PVtBxBNa7mD2EpA_7rA9ZjDAQDpPsfU.jpg?width=640&crop=smart&auto=webp&s=a49ffb84576197ab0692feaf58e84e389d140f76'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>React Native Complete Guide 2023</p>
+            <p className='font-mono text-md '>$16.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer' onClick={() => redirectToCoursesPage(2)}>
+            <img
+              key={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              src={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Web3 Programming With Moralis</p>
+            <p className='font-mono text-md '>$6.12</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer' onClick={() => redirectToCoursesPage(3)}>
+            <img
+              key={'https://miro.medium.com/v2/resize:fit:1080/1*NU0vBKtpCt9hfkVImj7keQ@2x.jpeg'}
+              src={'https://miro.medium.com/v2/resize:fit:1080/1*NU0vBKtpCt9hfkVImj7keQ@2x.jpeg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Fullstack blockchain Developer</p>
+            <p className='font-mono text-md '>$99.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://thumbs.dreamstime.com/b/c-programming-code-abstract-technology-background-computer-software-coding-d-c-programming-code-abstract-technology-background-219889203.jpg'}
+              src={'https://thumbs.dreamstime.com/b/c-programming-code-abstract-technology-background-computer-software-coding-d-c-programming-code-abstract-technology-background-219889203.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>C++ Fullsatck Dev</p>
+            <p className='font-mono text-md '>$47.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              src={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Web3 Programming With Moralis</p>
+            <p className='font-mono text-md '>$6.12</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://miro.medium.com/v2/resize:fit:1080/1*NU0vBKtpCt9hfkVImj7keQ@2x.jpeg'}
+              src={'https://miro.medium.com/v2/resize:fit:1080/1*NU0vBKtpCt9hfkVImj7keQ@2x.jpeg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Fullstack blockchain Developer</p>
+            <p className='font-mono text-md '>$99.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://thumbs.dreamstime.com/b/c-programming-code-abstract-technology-background-computer-software-coding-d-c-programming-code-abstract-technology-background-219889203.jpg'}
+              src={'https://thumbs.dreamstime.com/b/c-programming-code-abstract-technology-background-computer-software-coding-d-c-programming-code-abstract-technology-background-219889203.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>C++ Fullsatck Dev</p>
+            <p className='font-mono text-md '>$47.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              src={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Web3 Programming With Moralis</p>
+            <p className='font-mono text-md '>$6.12</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://thumbs.dreamstime.com/b/c-programming-code-abstract-technology-background-computer-software-coding-d-c-programming-code-abstract-technology-background-219889203.jpg'}
+              src={'https://thumbs.dreamstime.com/b/c-programming-code-abstract-technology-background-computer-software-coding-d-c-programming-code-abstract-technology-background-219889203.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>C++ Fullsatck Dev</p>
+            <p className='font-mono text-md '>$47.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://external-preview.redd.it/1GIbkRiU0HE6PVtBxBNa7mD2EpA_7rA9ZjDAQDpPsfU.jpg?width=640&crop=smart&auto=webp&s=a49ffb84576197ab0692feaf58e84e389d140f76'}
+              src={'https://external-preview.redd.it/1GIbkRiU0HE6PVtBxBNa7mD2EpA_7rA9ZjDAQDpPsfU.jpg?width=640&crop=smart&auto=webp&s=a49ffb84576197ab0692feaf58e84e389d140f76'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>React Native Complete Guide 2023</p>
+            <p className='font-mono text-md '>$16.99</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              src={'https://moralis.io/wp-content/uploads/2022/03/2022_02_How_to_Become_a_web3_developer_full_guide_V4.0.jpg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Web3 Programming With Moralis</p>
+            <p className='font-mono text-md '>$6.12</p>
+          </div>
+          <div className=' h-[20em] w-56 rounded-xl mt-16 mb-8 cursor-pointer'>
+            <img
+              key={'https://miro.medium.com/v2/resize:fit:1080/1*NU0vBKtpCt9hfkVImj7keQ@2x.jpeg'}
+              src={'https://miro.medium.com/v2/resize:fit:1080/1*NU0vBKtpCt9hfkVImj7keQ@2x.jpeg'}
+              alt="placeholder"
+              className='h-full rounded-3xl'
+            />
+            <p className='font-mono text-md '>Fullstack blockchain Developer</p>
+            <p className='font-mono text-md '>$99.99</p>
+          </div>
+
 
         </div>
       </div>
